ebcca63ef03592f725613233f6a61c5ddeb5021c
Stweer <38274756+Stweer@users.noreply.github.com>
2020-01-29 02:52:53 -0500

    Creacion de Actividad con su Layout "CompleteMap"

---
+++ app/src/main/java/com/example/u1quispemamaniejercicio1tema5/CompleteMap.java
@@ -0,0 +1,14 @@
+package com.example.u1quispemamaniejercicio1tema5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompleteMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_map);
+    }
+}
